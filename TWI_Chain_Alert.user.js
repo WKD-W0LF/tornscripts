a285728795495e91ae736f90ba2e8b6b77faeb82
WKD-W0LF <andrewbeattie@mac.com>
2026-07-19 21:27:10 +1000

    TWI_Chain_Alert v1.1.5 - add all 13 bonus numbers up to 100000

--- TWI_Chain_Alert.user.js
+++ TWI_Chain_Alert.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TWI Chain Alert
 // @namespace    twilight-reborn
-// @version      1.1.4
+// @version      1.1.5
 // @author       WKD-W0LF
 // @description  Chain bonus countdown alerts for Twilight-Reborn [56966]. Alerts at 5 hits from bonus, personalised banner for assigned hitters.
 // @license      MIT
@@ -23,7 +23,7 @@
   const TORN_API_BASE   = "https://api.torn.com";
   const ALLOWED_FACTION_ID = 56966;
   const ADMIN_IDS       = new Set(["3647423","3917106","3658650","3855001","3926412","4152155","4157019"]);
-  const BONUS_NUMBERS   = [10, 25, 50, 100, 250, 500, 1000];
+  const BONUS_NUMBERS   = [10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000];
   const POLL_MS         = 4000;
   const ASSIGN_POLL_MS  = 30000;   // re-fetch assignments every 30s
   const PREFIX          = "twi-chain-alert-";
